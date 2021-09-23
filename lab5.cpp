60b8c7d20e98e8338b38cb549745b2741bb72839
dimi34alex <dimi_alex@mail.ru>
2021-09-23 19:29:19 +0300

    Create lab5.cpp

---
+++ lab5.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    //Задача 1
+    int x1, y1, x2, y2;
+    cout << "Enter x1 and y1 \n";
+    cin >> x1 >> y1;
+    cout << "Enter x2 and y2 \n";
+    cin >> x2 >> y2;
+    cout << sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+
+    //Задача 2
+    int a, b, c;
+    cout << "Enter A, B, C \n";
+    cin >> a >> b >> c;
+    cout << "AC = " << c - a << "\nBC = " << c - b << "\nSumm = " << (c - a) + (c - b);
+
+    //Задача 3
+    cout << "Enter A, B, C \n";
+    cin >> a >> c >> b;
+    cout << (b - c) * (c - a);
+
+    // Задача 4
+    cout << "Enter x1 and y1 \n";
+    cin >> x1 >> y1;
+    cout << "Enter x2 and y2 \n";
+    cin >> x2 >> y2;
+    cout << "P = " << 2 * (abs(x1 - x2) + abs(y1 - y2)) << "\n";
+    cout << "S = " << abs(x1 - x2) * abs(y1 - y2) << "\n";
+
+    //Задача 5
+    int x1, y1, x2, y2;
+    int x3, y3;
+    float a, b, c;
+    cout << "Enter x1 and y1 \n";
+    cin >> x1 >> y1;
+    cout << "Enter x2 and y2 \n";
+    cin >> x2 >> y2;
+    cout << "Enter x3 and y3 \n";
+    cin >> x3 >> y3;
+    a = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+    b = sqrt(pow((x3 - x2), 2) + pow((y3 - y2), 2));
+    c = sqrt(pow((x3 - x1), 2) + pow((y3 - y1), 2));
+    cout << "P = " << a + b + c << "\n";
+    float p = (a + b + c) / 2;
+    cout << "S = " << sqrt(p * (p - a) * (p - b) * (p - c)) << "\n";
+    cout << a << "\n" << b << "\n" << c << "\n";
+}
