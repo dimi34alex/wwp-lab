567c264a791a86601fb31ade9139249aebb99a24
dimi34alex <dimi_alex@mail.ru>
2021-10-16 13:03:51 +0300

    Update lab4.cpp

--- lab4.cpp
+++ lab4.cpp
@@ -1,5 +1,4 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 int main()
@@ -23,17 +22,17 @@ int main()
     cout << "L = " << L;
 
     //Задача 3
-    cout << "Enter a and b"
-    cin >> a >> b;
+    cout << "Enter a and b";
+        cin >> a >> b;
     cout << (float)(a + b) / 2;
 
     //Задача 4
-    cout << "Enter 2 not null number"
-    cin >> a >> b;
-    cout << a + b << " " << a - b << " " << a * b << " " << (float)(a * a) / (b * b);
+    cout << "Enter 2 not null number";
+        cin >> a >> b;
+        cout << "Sum = " << a * a + b * b << "\n" << "Raz = " << a * a - b * b << "\n" << "Proiz = " << a * a * b * b << "\n" << "Chast = " << (float)(a * a) / (b * b);
 
     //Задача 5
-    cout << "Enter 2 not null number"
-    cin >> a >> b;
-    cout << a + b << " " << a - b << " " << a * b << " " << (float)abs(a) / abs(b);
+    cout << "Enter 2 not null number";
+        cin >> a >> b;
+        cout << "Sum = " << abs(a) + abs(b) << "\n" << "Raz = " << abs(a) - abs(b) << "\n" << "Proiz = " << abs(a) * abs(b) << "\n" << "Chast = " << (float)abs(a) / float(abs(b));
 }
